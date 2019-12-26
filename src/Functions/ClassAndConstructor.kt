d8e2bbed4ec1cd70ec46a7673cf5b016a057041b
poornima <poornima.tharma@anywhere.co>
2019-12-26 11:46:48 +0530

    testing init

--- src/Functions/ClassAndConstructor.kt
+++ src/Functions/ClassAndConstructor.kt
@@ -1,2 +1,15 @@
 package Functions
 
+class InitDemo constructor(arg1:String){
+    val firstString="demo number one".also (::println)
+    init {
+        "The argument is $arg1".also (::println)
+    }
+    init {
+        "Testing init block $arg1".also (::println)
+    }
+
+}
+fun main(){
+    InitDemo("Demo")
+}
