e3957d3b926ad9a423c3547e8dd787e765c04178
poornima <poornima.tharma@anywhere.co>
2020-02-12 12:26:14 +0530

    spread and vararg

---
+++ src/Functions/varargSample.kt
@@ -0,0 +1,48 @@
+package Functions
+
+open class varargSample {
+    companion object CallingInnerFunction {
+        var instance: varargSample? = null
+        val getInstance: varargSample
+            get() {
+                if (instance == null) {
+                    instance = varargSample()
+                }
+                return instance as varargSample
+            }
+    }
+
+    //if the varargs is not the last parameter then it has to be passed as named arguments
+
+    open fun callThisFunction(test:String,vararg arrayoFObj: Any,test1:String="test") {
+        for (arr in arrayoFObj){
+            println(arr)
+        }
+    }
+
+
+}
+
+class B: varargSample() {
+    override fun callThisFunction(test: String, vararg arrayoFObj: Any, test1: String) {
+        println(test1)
+        val list = arrayOf(1,2,3)
+        testingSpreadOperator(*list,"76")
+    }
+
+    fun testingSpreadOperator(vararg sample1:Any,sample2:Any=23){
+        for(sample in sample1){
+            print(sample)
+        }
+        println(sample2)
+    }
+}
+
+fun main() {
+    varargSample.getInstance.callThisFunction("ss",34,42,2,1,52,5,256)
+    var b=B()
+    b.callThisFunction("format", "test1","test2")
+    //b.testingVarargs(arrayOf(2,3,2),"sss")
+    //b.testingSpreadOperator(2,32,1,4)
+}
+
