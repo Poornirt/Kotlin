af7ad57afb030214e38f99960f468cb533003eca
poornima <poornima.tharma@anywhere.co>
2020-02-27 23:54:51 +0530

    iterator

---
+++ src/Collections/IteratorsDemo.kt
@@ -0,0 +1,8 @@
+package Collections
+
+fun main(){
+    val res= listOf<String>("3","3","2")
+    res.iterator().forEach {
+        println(it)
+    }
+}
---
+++ src/Collections/IteratorsDemo1.kt
@@ -0,0 +1,9 @@
+package Collections
+
+fun main(){
+    val display= listOf<String>("@ewrw","Werr","dfgdf")
+    val d=display.iterator();
+    while(d.hasNext()){
+        print(d.next())
+    }
+}
